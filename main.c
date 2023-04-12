1d8ca660296c0cae77e83ac72739a9b5a50bd2c8
Nathanlennon <nathaanbias@gmail.com>
2023-04-12 22:59:11 +0200

    résolution du probleme de compatibilité de windows qui est chiant avec unicode merci windows

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <windows.h> // ATTENTION SUPPR ÇA AVAIT DE RENDRE, C'EST POUR LES PROBLEMES DE COMPATIBILITÉ WINDOWS DE CLION
 
-int main(){
-    printf("issou");
+
+int main() {
+
+    srand(time(NULL));
+
+    // ATTENTION SUPPR ÇA AVAIT DE RENDRE, C'EST POUR LES PROBLEMES DE COMPATIBILITÉ WINDOWS DE CLION
+    #ifdef WIN32
+        SetConsoleOutputCP(65001);
+    #endif
 
     return 0;
 }
