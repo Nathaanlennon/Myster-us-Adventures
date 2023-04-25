79f898de11766106b696b27dcbe337729f8dc97f
schheeii <schheeii@gmail.com>
2023-04-25 16:31:54 +0200

    identations

---
+++ include/macro.h
@@ -0,0 +1,35 @@
+#define GRID_SIZE 5 //taille du labyrinthe
+#define BOARD_SIZE 7 //taille du plateau entier (avec les cases de départ incluses)
+
+#define HIDDEN "\u25A6"
+#define EMPTY "\u25A2"
+#define START "\u25A7"
+#define STICK "\u222B"
+#define DAGGER "\u2197"
+#define SWORD "\u2694"
+#define SPELLBOOK "\u2A70"
+#define CHEST "\u229F"
+#define TROLL "\u2689"
+#define BASILISK "\u267E"
+#define ZOMBIE "\u2623"
+#define HARPY "\u29EC"
+#define PORTAL "\u2145"
+#define TOTEM "\u25EE"
+
+#define C_BLK "\033[30m" // Caractère noir
+#define C_RED "\033[31m" // Caractère rouge
+#define C_GRN "\033[32m" // Caractère vert
+#define C_YEL "\033[33m" // Caractère jaune
+#define C_BLU "\033[34m" // Caractère bleu
+#define C_MAG "\033[35m" // Caractère magenta
+#define C_CYN "\033[36m" // Caractère cyan
+#define C_WHT "\033[37m" // Caractère blanc
+
+#define B_BLK "\033[40m" // Fond noir
+#define B_RED "\033[41m" // Fond rouge
+#define B_GRN "\033[42m" // Fond vert
+#define B_YEL "\033[43m" // Fond jaune
+#define B_BLU "\033[44m" // Fond bleu
+#define B_MAG "\033[45m" // Fond magenta
+#define B_CYN "\033[46m" // Fond cyan
+#define B_WHT "\033[47m" // Fond blanc
--- main.c
+++ main.c
@@ -2,22 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <windows.h> // ATTENTION SUPPR ÇA AVAIT DE RENDRE, C'EST POUR LES PROBLEMES DE COMPATIBILITÉ WINDOWS DE CLION
 
-#define BOARD_SIZE 5
-
-#define HIDDEN "\u25A6"
-#define STICK "\u222B"
-#define DAGGER "\u2197"
-#define SWORD "\u2694"
-#define SPELLBOOK "\u2A70"
-#define CHEST "\u229F"
-#define TROLL "\u2689"
-#define BASILISK "\u267E"
-#define ZOMBIE "\u2623"
-#define HARPY "\u29EC"
-#define PORTAL "\u2145"
-#define TOTEM "\u25EE"
+#include "include/macro.h"
+//#include "usual.c"
 
 // Structure pour une case du plateau de jeu
 typedef struct {
@@ -29,37 +16,46 @@ typedef struct {
 typedef struct {
     int number;
     char name[10];
-    Square progression[BOARD_SIZE][BOARD_SIZE]; //stocke la carte d'un joueur (gardant en mémoire les cases retournées)
+    int weapon;
 } Player;
 
 
 //Initialisation de la map aléatoire de symboles
-void initialize_map(Square **board, int size) {
+void initialize_map(Square **board, int boardSize, int gridSize) {
 
     char treasures[9][10] = {STICK, DAGGER, SWORD, SPELLBOOK, CHEST, CHEST, PORTAL, TOTEM,
                              TOTEM}; //Symboles des éléments au nombre précis à placer (trésors, portails, totem)
-    char monsters[4][10] = {TROLL, BASILISK, HARPY,
-                            ZOMBIE}; //Monstres qui rempliront le reste du labyrinthe de façon aléatoire
+    char monsters[4][10] = {ZOMBIE, BASILISK, TROLL,
+                            HARPY}; //Monstres qui rempliront le reste du labyrinthe de façon aléatoire
 
     //remplissage du labyrinthe par des monstres aléatoires et initialisation de l'état de base du plateau (cartes retournées et remplies)
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            strcpy(board[i][j].symbol, monsters[rand() % 4]);
-            board[i][j].flipped = 0;
+    for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
             board[i][j].emptied = 0;
+            board[i][j].flipped = 1;
+            if((i == 0 && j == boardSize-3) || (i == 2 && j == 0) || (i == boardSize-1 && j == 2) || (i == boardSize-3 && j == boardSize-1)){
+                strcpy(board[i][j].symbol, START);
+            }
+            else if((i%(boardSize-1) == 0) || (j%(boardSize-1) == 0)){
+                strcpy(board[i][j].symbol, " ");
+            }
+            else{
+                strcpy(board[i][j].symbol, monsters[rand() % 4]);
+                board[i][j].flipped = 0;
+            }
         }
     }
 
     int randomRow, randomCol;
 
-    //placement aléatoire des trésors (y'a moyen d'améliorer mais jsuis fatiguée)
+    //placement aléatoire des trésors
     for (int i = 0; i < 9; i++) {
-        randomRow = rand() % size;
-        randomCol = rand() % size;
+        randomRow = rand() % gridSize+1;
+        randomCol = rand() % gridSize+1;
         for (int k = 0; k < 9; k++) { //vérification si la coordonnée aléatoire n'est pas déjà l'emplacement d'un trésor
             if (strcmp(board[randomRow][randomCol].symbol, treasures[k]) == 0) {
-                randomRow = rand() % size;
-                randomCol = rand() % size;
+                randomRow = rand() % gridSize+1;
+                randomCol = rand() % gridSize+1;
             }
         }
         strcpy(board[randomRow][randomCol].symbol, treasures[i]);
@@ -67,22 +63,21 @@ void initialize_map(Square **board, int size) {
 }
 
 //Création et initialisation d'un tableau 2D de structures Square <=> création d'un plateau de jeu
-Square **create_board(int size) {
-    Square **board = malloc(size * sizeof(Square *));
+Square **create_board(int boardSize, int gridSize) {
+    Square **board = malloc(boardSize * sizeof(Square *));
     if (board == NULL) { //gérer si l'allocation a échoué
         exit(1);
     }
 
     //pour chaque ligne du plateau
-    for (int i = 0; i < size; i++) {
-        board[i] = malloc(size *
-                          sizeof(Square)); //allouer de l'espace pour chacune des 5 lignes du plateau (pour chacun des 5 tableaux de 5 char*)
+    for (int i = 0; i < boardSize; i++) {
+        board[i] = malloc(boardSize * sizeof(Square)); //allouer de l'espace pour chacune des lignes du plateau (pour chacun des tableaux de char*)
         if (board[i] == NULL) { //gérer si l'allocation a échoué
             exit(1);
         }
     }
 
-    initialize_map(board, size);
+    initialize_map(board, boardSize, gridSize);
 
     return board;
 }
@@ -96,9 +91,9 @@ void free_board(Square **board, int size) {
 }
 
 //Afficher le plateau
-void print_board(Square **board, int size) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+void print_board(Square **board, int boardSize) {
+    for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
             if (board[i][j].flipped == 1) {
                 printf("%s ", board[i][j].symbol);
             } else {
@@ -109,12 +104,12 @@ void print_board(Square **board, int size) {
     }
 }
 
-void flip_card(Square **board, int size) {
+void flip_card(Square **board, int boardSize, int gridSize) {
     int x, y;
     do {
         printf("Retourner quelle carte ? donner coordonnées x, y : ");
         scanf("%d, %d", &x, &y);
-    } while (x < 0 || x > 4 || y < 0 || y > 4);
+    } while (x < boardSize-1-gridSize || x > gridSize || y < 1 || y > boardSize-1-gridSize);
 
     board[x][y].flipped = 1;
 }
@@ -124,21 +119,15 @@ int main() {
 
     srand(time(NULL));
 
-    // ATTENTION SUPPR ÇA AVAIT DE RENDRE, C'EST POUR LES PROBLEMES DE COMPATIBILITÉ WINDOWS DE CLION
-    #ifdef WIN32
-    SetConsoleOutputCP(65001);
-    #endif
-    //fin
-
     ////////////        CREATION ET INITIALISATION DU PLATEAU DE CASES ////////////
-    Square **board = create_board(BOARD_SIZE);
+    Square **board = create_board(BOARD_SIZE, GRID_SIZE);
 
     ////////////        AFFICHAGE        ////////////
     print_board(board, BOARD_SIZE);
 
     //test de flip_card
     for (int i = 0; i < 5; i++) {
-        flip_card(board, BOARD_SIZE);
+        flip_card(board, BOARD_SIZE, GRID_SIZE);
         print_board(board, BOARD_SIZE);
     }
 
