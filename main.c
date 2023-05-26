7aaadba253dd1d5d870ff9c3081193b034305c1e
schheeii <schheeii@gmail.com>
2023-05-26 21:36:59 +0200

    Déplacement v2 avec un choix de direction plutôt que de coordonnées + autre fonction nécessaire à son fonctionnement ajoutée
    Problème avec getint dans le choix des armes, pour l'instant réglé avec un scanf
    
    Taille limite de pseudo du joueur fixée à 30
    
    Ajouts de commentaires

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ typedef struct {
     int ancientWeapon_found; //booléen si l'arme antique a été trouvée (condition nécessaire à la victoire)
     int treasure_found; //booléen si au moins 1 trésor a été trouvé (condition nécessaire à la victoire)
 
-    char name[];
+    char name[30];
     //color mais j'ai la flemme pour l'instant
 } Player;
 
@@ -324,7 +324,6 @@ void print_board_admin(Square **board, int boardSize, Player* player) { //print 
 }
 
 void weapon_choice(Player *player) {
-
     if(player == NULL){
         write_crash_report("pointer in parameters is NULL");
         exit(1);
@@ -333,24 +332,29 @@ void weapon_choice(Player *player) {
     printf("Choisissez une arme pour vous préparer à un potentiel combat...\n");
     printf("1 : Torche\n2 : Bouclier réfléchissant\n3 : Hâche\n4 : Arc\n");
     int i = 0; // i est le nombre d'erreurs que le joueur commet. C'est-à-dire quand ils donnent autre chose que demandé
-    player->weapon = getint()-1;
+
+    scanf("%d", &(player->weapon));
+    //player->weapon = getint()-1;  PROBLEME
     flush_input_buffer();
+    player->weapon -= 1;
     while (player->weapon < 0 || player->weapon > 3) {
         i++; // on incrémente le nombre d'erreurs
         if (i >= 3) { //trop d'erreurs alors le jeu prend des mesures
             printf("Ok si vous insistez...pas d'armes...\n");
-            player->weapon = 0;
+            player->weapon = -1;
             waiting();
             break;
         }
         cursor_move('A', 1); //déplace le curseur d'une case vers le haut pour remplacer la dernière ligne
         printf("Bien essayé mais vous n'avez pas d'autres armes.\n");
-        player->weapon = getint();
+        //player->weapon = getint()-1; PROBLEME
+        scanf("%d", &(player->weapon));
         flush_input_buffer();
+        player->weapon -= 1;
     }
 }
 
-//Choisir des coordonnées x et y ne sortant pas du tableau (allant de 0 à 6). Sera utilisée pour la fonction de mouvement, pour la téléportation, pour le totem de transmutation.
+//Choisir des coordonnées x et y ne sortant pas du tableau (allant de 0 à 6). Sera utilisée pour la téléportation et pour le totem de transmutation.
 void choose_coordinates(Square **board, int* x, int* y, int boardSize, int gridSize) {
 
     if(board == NULL || x == NULL ||y == NULL){
@@ -361,9 +365,80 @@ void choose_coordinates(Square **board, int* x, int* y, int boardSize, int gridS
     printf("\nChoisir une case en entrant les numéros de la ligne,colonne : ");
     scanf("%d,%d", x, y);
     flush_input_buffer();
-    if (*x < boardSize - 1 - gridSize || *x > gridSize || *y < boardSize - 1 - gridSize || *y > gridSize || board[*x][*y].flipped == 1) { //vérification que les coordonnées saisies ne soint pas hors du tableau ou une case déjà vidée
+    //vérification que les coordonnées saisies ne soient pas hors du tableau ou une case déjà retournée.
+    if (*x < boardSize - 1 - gridSize || *x > gridSize || *y < boardSize - 1 - gridSize || *y > gridSize || board[*x][*y].flipped == 1) {
         printf("Coordonnées invalides.");
         choose_coordinates(board, x, y, boardSize, gridSize);
+    }
+}
+
+//Vérifie si la case entrée est déjà retournée, ou est le bord de la map, ou est une case de départ. Renvoie 1 si elle est l'une de ces conditions, 0 sinon.
+int CheckSquareInvalid(Square square) {
+    return (square.flipped == 1 || strcmp(square.symbol, " ") == 0 || strcmp(square.symbol, START) == 0);
+}
+
+//Vérifie si l'index spécifié est valide pour être recherché dans un tableau. Renvoie 1 si hors du tableau, 0 sinon.
+int CheckIndexOutOfArray(int i, int arraySize){
+    return (i < 0 || i > arraySize);
+}
+
+//Fonction de choix de direction dans laquelle se déplacer
+void choose_direction(Square **board, Player* player, int* x, int* y, int boardSize, int gridSize) {
+    if(board == NULL || player == NULL || x == NULL ||y == NULL){
+        write_crash_report("pointer in parameters is NULL");
+        exit(1);
+    }
+
+    printf("Choisir une direction :\n");
+
+    //vérifie les directions possibles à proposer
+    int validUp = (CheckIndexOutOfArray(player->position_x-1, boardSize)) ? 0 : !CheckSquareInvalid(board[player->position_x-1][player->position_y]);
+    int validLeft = (CheckIndexOutOfArray(player->position_y-1, boardSize)) ? 0 : !CheckSquareInvalid(board[player->position_x][player->position_y-1]);
+    int validRight = (CheckIndexOutOfArray(player->position_y+1, boardSize)) ? 0 : !CheckSquareInvalid(board[player->position_x][player->position_y+1]);
+    int validDown = (CheckIndexOutOfArray(player->position_x+1, boardSize)) ? 0 : !CheckSquareInvalid(board[player->position_x+1][player->position_y]);
+
+    if(validUp){
+        printf("Haut\n[8]\n");
+    }
+    if(validLeft){
+        printf("Gauche [4]");
+    }
+    if(validRight){
+        printf(" [6] Droite");
+    }
+    if(validDown){
+        printf("\n[2]\nBas");
+    }
+
+    printf("\n");
+    int input;
+    scanf("%d",&input);
+
+    //vérification que la direction entrée soit valide
+    while ((input != 8 || validUp == 0) && (input != 6 || validRight == 0) && (input != 4 || validLeft == 0) && (input != 2 || validDown == 0)) {
+        printf("Direction invalide.\n");
+        scanf("%d",&input);
+        flush_input_buffer();
+    }
+
+    //envoie au reste du programme les coordonnées vers lesquelles le joueur veut se déplacer
+    switch(input){
+        case 8:
+            *x = player->position_x-1;
+            *y = player->position_y;
+            break;
+        case 6:
+            *x = player->position_x;
+            *y = player->position_y+1;
+            break;
+        case 4:
+            *x = player->position_x;
+            *y = player->position_y-1;
+            break;
+        case 2:
+            *x = player->position_x+1;
+            *y = player->position_y;
+            break;
     }
 }
 
@@ -462,22 +537,17 @@ int event_manager(int* x, int* y, Square **board, int boardSize, int gridSize, P
     return 1;
 }
 
-//Vérifie si la case entrée est vidée (monstre a été tué), est le bord de la map, ou est une case de départ). Renvoie 1 si elle est l'une de ces conditions, 0 sinon.
-int CheckSquareInvalid(Square square) {
-    return (square.flipped == 1 || strcmp(square.symbol, " ") == 0 || strcmp(square.symbol, START) == 0);
-}
-
 //reset du plateau à la mort du joueur ou si le joueur est bloqué
 void reset(Square **board, int boardSize, int gridSize, Player* player){
     for(int i = 1; i <= gridSize; i++){
         for (int j = 1; j <= gridSize; j++){
-            board[i][j].flipped = 0;
-            board[i][j].emptied = 0;
+            board[i][j].flipped = 0; //retourne les cartes
+            board[i][j].emptied = 0; //les monstres reviennent à la vie
         }
     }
     player->position_x = player->start_x; //le joueur est remis à sa position de départ
     player->position_y = player->start_y;
-    player->treasure_found = 0;
+    player->treasure_found = 0; //il doit retrouver son trésor et son arme à nouveau
     player->ancientWeapon_found = 0;
     printf("Vous revenez à votre position de départ...\n");
     print_board(board, boardSize, player);
@@ -491,24 +561,16 @@ void turn(Square **board, int boardSize, int gridSize, Player* player, const cha
         exit(1);
     }
 
-    weapon_choice(player);
-
+    weapon_choice(player); // choix de l'arme
     int x, y;
-    choose_coordinates(board, &x, &y, boardSize, gridSize); //saisie des coordonnées vers où se déplacer
-    while(abs(x - player->position_x) > 1 //conditions pour n'avancer que d'une case adjacente à la position actuelle
-            || abs(y - player->position_y) > 1
-            || (x != player->position_x && y != player->position_y) //ne pas aller en diagonale
-            || (x == player->position_x && y == player->position_y))//ne pas faire du sur place
-    {
-        printf("Coordonnées invalides.");
-        choose_coordinates(board, &x, &y, boardSize, gridSize);
-    }
+    choose_direction(board, player, &x, &y, boardSize, gridSize); //choix de la direction dans laquelle se déplacer
 
-    if(event_manager(&x, &y, board, boardSize, gridSize, player, monsters, weapons, treasures)){ //déplacer le joueur sur la case choisie si il n'est pas mort
+    if(event_manager(&x, &y, board, boardSize, gridSize, player, monsters, weapons, treasures)){ //déplacer le joueur sur la case choisie si il n'est pas mort et peut rejouer
         player->position_x = x;
         player->position_y = y;
         //player case +1
 
+        //avant de rejouer, vérification si le joueur est bloqué dans le labyrinthe
         if(CheckSquareInvalid(board[player->position_x+1][player->position_y]) && CheckSquareInvalid(board[player->position_x-1][player->position_y])
              && CheckSquareInvalid(board[player->position_x][player->position_y+1]) && CheckSquareInvalid(board[player->position_x][player->position_y-1])){
             print_board(board, boardSize, player);
@@ -520,7 +582,7 @@ void turn(Square **board, int boardSize, int gridSize, Player* player, const cha
             turn(board, boardSize, gridSize, player, monsters, weapons, treasures);
         }
     }
-    else{
+    else{ //cas où le joueur est mort ou a utilisé un totem de transmutation, ce qui réinitialise sa position et réinitialise la carte
         reset(board, boardSize, gridSize, player);
     }
 }
